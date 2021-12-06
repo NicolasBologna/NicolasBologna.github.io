58e0485f21b5a7b8c8673603a8f0e2af59c02c15
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2021-12-05 23:46:16 -0300

    first final version

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const dinamicURL = 'https://www.dinamicstudio.com/';
 const pwcURL = 'https://www.pwc.com/';
-function redirect(site){window.location.href = site} 
+function redirect(site){window.open(site, 'blank');} 
+
+window.addEventListener("scroll", function() {
+    var elementTarget = document.getElementById("portfolio-section");
+    if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+        elementTarget.getElementsByTagName("h4")[0].classList.add('loading');
+    }
+  });
