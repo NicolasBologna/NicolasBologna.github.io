b9077708a0357578c25980ee332b596f53d2f3be
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2021-12-05 22:46:58 -0300

    initial commit for final idea

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+const dinamicURL = 'https://www.dinamicstudio.com/';
+const pwcURL = 'https://www.pwc.com/';
+function redirect(site){window.location.href = site} 
