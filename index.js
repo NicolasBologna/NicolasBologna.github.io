16d8ab601a3eaf901000e93afed4e6fa195e7cf6
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2021-12-06 00:00:19 -0300

    loading animation

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 const dinamicURL = 'https://www.dinamicstudio.com/';
 const pwcURL = 'https://www.pwc.com/';
 function redirect(site){window.open(site, 'blank');} 
-
-window.addEventListener("scroll", function() {
-    var elementTarget = document.getElementById("portfolio-section");
-    if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
-        elementTarget.getElementsByTagName("h4")[0].classList.add('loading');
-    }
-  });
